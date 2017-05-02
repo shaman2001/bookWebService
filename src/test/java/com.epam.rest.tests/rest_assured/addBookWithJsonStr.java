0823564709f2d0433dc9ed778c6ee7a59dac0831
shaman2001 <Vladimir_Kotovich@epam.com>
2017-05-02 13:44:28 +0300

    new PUT method's tests when hardcoded JSON string is sent as request body

---
+++ src/test/java/com.epam.rest.tests/rest_assured/addBookWithJsonStr.java
@@ -0,0 +1,37 @@
+package com.epam.rest.tests.rest_assured;
+
+import com.jayway.restassured.http.ContentType;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.epam.rest.constants.CommonConstants.*;
+
+
+public class addBookWithJsonStr extends BaseTest{
+
+    public final static String WRONG_JSON_OBJECT_STR = "{\"id\": 6, \"name\": \"The oldman and the sea\", " +
+                "\"genre\": \"novel\", \"author\": \"Ernest Hamingway\", \"year_of_writing\": 1955, " +
+                "\"locallink\": \"ftp://localhost:25/pub/the_oldman_and_the_sea.txt\"}";
+
+    public final static String PROPER_JSON_OBJECT_STR = "{\"id\": 6, \"name\": \"The oldman and the sea\", " +
+            "\"genre\": \"novel\", \"author\": \"Ernest Hamingway\", \"year_of_issue\": 1955, " +
+            "\"link\": \"ftp://localhost:25/pub/the_oldman_and_the_sea.txt\"}";
+
+    @Test
+    public void addWrongBookStr () {
+        int bookCountBefore = Integer.parseInt(given().when().get().then().extract().header(BOOKS_COUNT));
+        given().contentType(ContentType.JSON).body(WRONG_JSON_OBJECT_STR).when().put().then().statusCode(500);
+        int bookCountAfter = Integer.parseInt(given().when().get().then().extract().header(BOOKS_COUNT));
+        Assert.assertTrue(bookCountBefore == bookCountAfter);
+    }
+
+    @Test
+    public void addProperBookStr () {
+        int bookCountBefore = Integer.parseInt(given().when().get().then().extract().header(BOOKS_COUNT));
+        given().contentType(ContentType.JSON).body(PROPER_JSON_OBJECT_STR).when().put().then().statusCode(200);
+        int bookCountAfter = Integer.parseInt(given().when().get().then().extract().header(BOOKS_COUNT));
+        Assert.assertFalse(bookCountBefore == bookCountAfter);
+    }
+
+}
